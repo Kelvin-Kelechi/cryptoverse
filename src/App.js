0b6b54b8aa5945bef60c02333729c0ab078d88ca
KELEX <ogkelvin99@gmail.com>
2023-05-22 19:43:21 +0100

    breaktime

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import { Route, Routes } from "react-router-dom";
 import { Coin, Home } from "./pages";
 import { Navbar } from "./components";
 import { useStyles } from "./styles";
- 
+import { ThemeProvider } from "@mui/styles";
+import { createTheme } from "@mui/material";
+const theme = createTheme();
 function App() {
- const classes = useStyles()
+  const classes = useStyles();
   return (
-    <div className={classes.app}>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/coins/:id" element={<Coin />} />
-      </Routes>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className={classes.app}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/coins/:id" element={<Coin />} />
+        </Routes>
+      </div>
+    </ThemeProvider>
   );
 }
 
--- src/pages/CoinPage/Coin.jsx
+++ src/pages/CoinPage/Coin.jsx
@@ -3,21 +3,101 @@ import { useParams } from "react-router-dom";
 import { CryptoState } from "../../CryptoContext";
 import axios from "axios";
 import { SingleCoin } from "../../components/config/api";
+import CoinInfo from "../../components/CoinInfo/CoinInfo";
+import { useCoinStyles } from "./styles";
+import { Typography } from "@mui/material";
+import ReactHtmlParser from "react-html-parser";
+import { numberWithCommas } from "../../components/CoinTable/CoinTable";
 
 const Coin = () => {
+  const classes = useCoinStyles();
   const { id } = useParams();
   const [coin, setCoin] = useState();
   const { currency, symbol } = CryptoState();
   const fetchSingleCoin = async () => {
     const { data } = await axios.get(SingleCoin(id));
     setCoin(data);
-    console.log(data)
+    console.log(data);
   };
   useEffect(() => {
     fetchSingleCoin();
   }, []);
 
-  return <div>Coin</div>;
+   
+
+  return (
+    <div className={classes.container}>
+      <div className={classes.sidebar}>
+        <img
+          src={coin?.image.large}
+          alt={coin?.name}
+          style={{ marginBottom: 20 }}
+          height="200"
+        />
+        <Typography variant="h3" className={classes.heading}>
+          {coin?.name}
+        </Typography>
+        <Typography variant="subtitle1" className={classes.description}>
+          {coin?.description.en.split(". ")[0]}.
+        </Typography>
+        <div className={classes.marketData}>
+        <span style={{ display: "flex" }}>
+            <Typography variant="h5" className={classes.heading}>
+              Rank:
+            </Typography>
+            &nbsp; &nbsp;
+            <Typography
+              variant="h5"
+              style={{
+                fontFamily: "Montserrat",
+              }}
+            >
+              {numberWithCommas(coin?.market_cap_rank)}
+            </Typography>
+          </span>
+
+          <span style={{ display: "flex" }}>
+            <Typography variant="h5" className={classes.heading}>
+              Current Price:
+            </Typography>
+            &nbsp; &nbsp;
+            <Typography
+              variant="h5"
+              style={{
+                fontFamily: "Montserrat",
+              }}
+            >
+              {symbol}{" "}
+              {numberWithCommas(
+                coin?.market_data.current_price[currency.toLowerCase()]
+              )}
+            </Typography>
+          </span>
+          <span style={{ display: "flex" }}>
+            <Typography variant="h5" className={classes.heading}>
+              Market Cap:
+            </Typography>
+            &nbsp; &nbsp;
+            <Typography
+              variant="h5"
+              style={{
+                fontFamily: "Montserrat",
+              }}
+            >
+              {symbol}{" "}
+              {numberWithCommas(
+                coin?.market_data.market_cap[currency.toLowerCase()]
+                  .toString()
+                  .slice(0, -6)
+              )}
+              M
+            </Typography>
+          </span>
+        </div>
+      </div>
+      <CoinInfo coin={coin} />
+    </div>
+  );
 };
 
 export default Coin;
