daa20502395137a199a2c99f9f9a08a3a03060d5
KELEX <ogkelvin99@gmail.com>
2023-05-22 16:29:38 +0100

    creating single coin

--- src/pages/CoinPage/Coin.jsx
+++ src/pages/CoinPage/Coin.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { CryptoState } from "../../CryptoContext";
+import axios from "axios";
+import { SingleCoin } from "../../components/config/api";
 
 const Coin = () => {
-  return (
-    <div>Coin</div>
-  )
-}
+  const { id } = useParams();
+  const [coin, setCoin] = useState();
+  const { currency, symbol } = CryptoState();
+  const fetchSingleCoin = async () => {
+    const { data } = await axios.get(SingleCoin(id));
+    setCoin(data);
+    console.log(data)
+  };
+  useEffect(() => {
+    fetchSingleCoin();
+  }, []);
 
-export default Coin
+  return <div>Coin</div>;
+};
+
+export default Coin;
