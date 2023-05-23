8fd94732fc23a93e7ede754f3f0e00bf3aa06897
KELEX <ogkelvin99@gmail.com>
2023-05-23 01:37:42 +0100

    working on coinInfo

--- src/components/CoinInfo/CoinInfo.jsx
+++ src/components/CoinInfo/CoinInfo.jsx
@@ -4,7 +4,8 @@ import { HistoricalChart } from "../config/api";
 import { CryptoState } from "../../CryptoContext";
 import { ThemeProvider } from "@emotion/react";
 import { useCoinInfoStyles } from "./styles";
-import { createTheme } from "@mui/material";
+import { CircularProgress, createTheme } from "@mui/material";
+import { Line } from "react-chartjs-2";
 
 const CoinInfo = ({ coin }) => {
   const [days, setDays] = useState(1);
@@ -13,9 +14,9 @@ const CoinInfo = ({ coin }) => {
   const { currency } = CryptoState();
   const fetchHistoricalChart = async () => {
     const { data } = await axios.get(HistoricalChart(coin.id, days, currency));
-    setHistorical(data.price);
+    setHistorical(data.prices);
   };
-
+   console.log("data", historical);
   useEffect(() => {
     fetchHistoricalChart();
   }, [currency, days]);
@@ -30,7 +31,25 @@ const CoinInfo = ({ coin }) => {
   });
   return (
     <ThemeProvider theme={darkTheme}>
-      <div className={classes.container}></div>{" "}
+      <div className={classes.container}>
+        {!historical ? (
+          <CircularProgress
+            thickness={1}
+            size={250}
+            style={{ color: "#56F21B" }}
+          />
+        ) : (
+          <>
+            <Line
+              data={{
+                 labels: historical.map((coin)=>{
+
+                 })
+              }}
+            />
+          </>
+        )}
+      </div>{" "}
     </ThemeProvider>
   );
 };
